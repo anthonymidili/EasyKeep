d9646597971499b1b42bae9c374a6f3617d5ce20
Anthony M. Midili <tonywinslow@yahoo.com>
2013-06-25 21:11:25 -0400

    Remove ability to sign_up from devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 HouseKeeping::Application.routes.draw do
 
-  devise_for :users
-
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   root to: 'welcome#home'
 
   # The priority is based upon order of creation:
