3eb1c945cdfb606fb9d6d1e3acbcc15a8fe00f96
Anthony M. Midili <tonywinslow@yahoo.com>
2013-06-30 13:18:06 -0400

    Add validations to account model. Add foundation styling to shared error messages.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,16 @@
 class Account < ActiveRecord::Base
   attr_accessible :address_1, :address_2, :city, :email, :fax, :name, :phone, :state, :zip
 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :address_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :phone, presence: true
+
   def full_address
     address_2.blank? ? "#{address_1}" : "#{address_1}, #{address_2}"
   end
+
 end
