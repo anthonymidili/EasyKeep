e89c81cd04a95fc83f5533ea12f52b516e3a9ce3
Anthony M. Midili <tonywinslow@yahoo.com>
2013-07-05 19:55:42 -0400

    Mock company views.

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,12 @@ class Company < ActiveRecord::Base
   attr_accessible :address_1, :address_2, :city, :company_name, :fax, :phone, :state, :zip
 
   belongs_to :admin
+
+  validates :company_name, presence: true
+  validates :address_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :phone, presence: true
+
 end
