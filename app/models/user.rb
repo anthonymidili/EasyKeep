53724e0d41b3a1bda2cc0abc36a8d62e6296efb2
Anthony M. Midili <tonywinslow@yahoo.com>
2013-07-18 23:18:52 -0400

    Validates email uniqueness, format and password length. Still need to validate presence of both except when in the accounts controller!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,12 @@ class User < ActiveRecord::Base
   belongs_to :company
 
   # validates_presence_of   :email, :if => :email_required?
-  # validates_uniqueness_of :email, :allow_blank => true, :if => :email_changed?
-  # validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+  validates_uniqueness_of :email, :allow_blank => true, :if => :email_changed?
+  validates_format_of     :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
 
   # validates_presence_of     :password, :if => :password_required?
   # validates_confirmation_of :password, :if => :password_required?
-  # validates_length_of       :password, :within => Devise.password_length, :allow_blank => true
+  validates_length_of       :password, :within => Devise.password_length, :allow_blank => true
 
   # def skip_password!
   #   @skip_password = true
