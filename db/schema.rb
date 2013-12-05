2ae18fa2a84464d78244056908bf5b7aa82df0d8
Anthony M. Midili <tonywinslow@yahoo.com>
2013-12-05 16:50:13 -0500

    Remove default 60 to users table, invatation_token.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131204215429) do
+ActiveRecord::Schema.define(:version => 20131205214600) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -52,26 +52,26 @@ ActiveRecord::Schema.define(:version => 20131204215429) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.string   "encrypted_password",                   :default => ""
+    t.string   "encrypted_password",     :default => ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                              :null => false
-    t.datetime "updated_at",                                              :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.integer  "company_id"
-    t.string   "invitation_token",       :limit => 60
+    t.string   "invitation_token"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.string   "name"
-    t.boolean  "is_admin",                             :default => false
+    t.boolean  "is_admin",               :default => false
     t.datetime "invitation_created_at"
   end
 
