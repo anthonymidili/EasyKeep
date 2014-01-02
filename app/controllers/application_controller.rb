0d747da7f823a1e74f798d9271d72666646725bf
Anthony M. Midili <tonywinslow@yahoo.com>
2014-01-02 16:33:57 -0500

    Complete select year function without rescue.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_cookies
 
   def current_company
     current_user.company if user_signed_in?
   end
 
   def active_date
-    @active_date = begin
+    if params[:date]
       cookies[:active_date] = Date.new(params[:date][:year].to_i,
                                        params[:date][:month].to_i,
                                        params[:date][:day].to_i)
-    rescue
-      cookies[:active_date].nil? ?
-          cookies[:active_date] = Date.current :
-          cookies[:active_date].to_date
+    else
+      cookies[:active_date].to_date
     end
   end; helper_method :active_date
+
+private
+
+  def set_cookies
+    cookies[:active_date] = Date.current if cookies[:active].nil?
+  end
 end
