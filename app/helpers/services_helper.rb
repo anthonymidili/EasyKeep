3f24d3e86ad7a205f5f72a5ff6db1f8b5f60f2b3
Anthony M. Midili <tonywinslow@yahoo.com>
2014-01-04 12:00:46 -0500

    Fix pagination. Paginate services with by_year or by_month scope.

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,9 +1,19 @@
 module ServicesHelper
   def year_or_month_select
-    if view_by == :year
-      select_date(active_date, discard_month: true, discard_day: true)
-    else
-      select_date(active_date, discard_day: true)
+    case view_by
+      when :year
+        select_date(active_date, discard_month: true, discard_day: true)
+      else
+        select_date(active_date, discard_day: true)
+    end
+  end
+
+  def paginate_with_scope
+    case view_by
+      when :year
+        @services.by_year(active_date)
+      else
+        @services.by_month(active_date)
     end
   end
 end
