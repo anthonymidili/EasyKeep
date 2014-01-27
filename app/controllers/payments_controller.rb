f1d04ee3d0911b5929037dfefa4e13bb14fd92a9
Anthony M. Midili <tonywinslow@yahoo.com>
2014-01-26 21:03:40 -0500

    Change links and form for nested routes.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,83 +1,54 @@
 class PaymentsController < ApplicationController
-  # GET /payments
-  # GET /payments.json
+  before_filter :authenticate_user!
+  before_filter :require_admin!
+  before_filter :load_invoice
+
   def index
-    @payments = Payment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @payments }
-    end
+    @payments = @invoice.payments
   end
 
-  # GET /payments/1
-  # GET /payments/1.json
   def show
-    @payment = Payment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @payment }
-    end
+    @payment = @invoice.payments.find(params[:id])
   end
 
-  # GET /payments/new
-  # GET /payments/new.json
   def new
-    @payment = Payment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @payment }
-    end
+    @payment = @invoice.payments.build
   end
 
-  # GET /payments/1/edit
-  def edit
-    @payment = Payment.find(params[:id])
-  end
-
-  # POST /payments
-  # POST /payments.json
   def create
-    @payment = Payment.new(params[:payment])
+    @payment = @invoice.payments.build(params[:payment])
 
-    respond_to do |format|
-      if @payment.save
-        format.html { redirect_to @payment, notice: 'Payment was successfully created.' }
-        format.json { render json: @payment, status: :created, location: @payment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @payment.errors, status: :unprocessable_entity }
-      end
+    if @payment.save
+      redirect_to @invoice, notice: 'Payment was successfully created.'
+    else
+      render 'invoices/show'
     end
   end
 
-  # PUT /payments/1
-  # PUT /payments/1.json
+  def edit
+    @payment = @invoice.payments.find(params[:id])
+  end
+
   def update
-    @payment = Payment.find(params[:id])
+    @payment = @invoice.payments.find(params[:id])
 
-    respond_to do |format|
-      if @payment.update_attributes(params[:payment])
-        format.html { redirect_to @payment, notice: 'Payment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @payment.errors, status: :unprocessable_entity }
-      end
+    if @payment.update_attributes(params[:payment])
+      redirect_to invoice_payment_path(@invoice, @payment), notice: 'Payment was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
-  # DELETE /payments/1
-  # DELETE /payments/1.json
   def destroy
-    @payment = Payment.find(params[:id])
+    @payment = @invoice.payments.find(params[:id])
     @payment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to payments_url }
-      format.json { head :no_content }
-    end
+    redirect_to invoice_payments_path(@invoice)
+  end
+
+private
+
+  def load_invoice
+    @invoice = current_account.invoices.find(params[:invoice_id])
   end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,7 @@
 class Payment < ActiveRecord::Base
-  attr_accessible :amount, :invoice_id, :received_on, :type
+  attr_accessible :amount, :received_on, :type
 
   belongs_to :invoice
+
+  validates :amount, presence: true
 end
