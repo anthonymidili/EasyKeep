2dc044469ca182e692d1d3a4b30986a9fd84b9cc
Anthony M. Midili <tonywinslow@yahoo.com>
2014-02-02 00:00:27 -0500

    Display company owners name and email and admins names.

--- app/models/company.rb
+++ app/models/company.rb
@@ -15,7 +15,15 @@ class Company < ActiveRecord::Base
     [ address_1, address_2, city, state, zip ].select(&:present?).join(', ')
   end
 
-  def admins
+  def by_owner_name
+    users.where(is_owner: true).map(&:name).join(', ')
+  end
+
+  def by_owner_email
+    users.where(is_owner: true).map(&:email).join(', ')
+  end
+
+  def by_admin
     users.where(is_admin: true)
   end
 end
