cf84c7471c8b6af125fc7cf03fbd639f12126406
Anthony M. Midili <tonywinslow@yahoo.com>
2014-02-04 20:52:20 -0500

    Ignore secret env file.

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => "house-keeping.herokuapp.com",
-    :address => "smtp.sendgrid.net",
+    :domain => 'donnagibbs.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
