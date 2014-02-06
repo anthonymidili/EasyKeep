3fd518b4609e31dfa2ca2564c9deb233f3e5b280
Anthony M. Midili <tonywinslow@yahoo.com>
2014-02-05 21:53:47 -0500

    Add custom validation. Payment can't be greater than the amount owed.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,10 +4,19 @@ class Payment < ActiveRecord::Base
   belongs_to :invoice
 
   validates :amount, presence: true, numericality: true
+  validate :no_credit_allowed
   validates :transaction_type, presence: true
   validates :received_on,
             format: { with: /^(?<year>\d{4})\-(?<month>\d{1,2})\-(?<day>\d{1,2})$/,
                       message: 'date must be formatted correctly (yyyy-mm-dd)' }
 
   default_scope order: 'received_on DESC'
+
+private
+
+  def no_credit_allowed
+    if amount > invoice.money_owed
+      errors.add(:amount, "can't be greater than total money owed")
+    end
+  end
 end
