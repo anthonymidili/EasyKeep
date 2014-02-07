637f1ddc792675aac151835a796bfa57794e519f
Anthony M. Midili <tonywinslow@yahoo.com>
2014-02-06 21:52:16 -0500

    Test

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'donnagibbs.herokuapp.com',
+    :domain => 'house-keeping.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
