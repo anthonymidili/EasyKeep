1926141c7db4f948e9d74ea71a6c6da47141d156
Anthony M. Midili <tonywinslow@yahoo.com>
2014-02-09 15:59:29 -0500

    Hide environment.rb vars.

--- config/environment.rb
+++ config/environment.rb
@@ -2,13 +2,13 @@
 require File.expand_path('../application', __FILE__)
 
 ActionMailer::Base.smtp_settings = {
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'donnagibbs.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    :user_name =>           ENV["SENDGRID_USERNAME"],
+    :password =>            ENV["SENDGRID_PASSWORD"],
+    :domain =>              ENV['SENDGRID_DOMAIN'],
+    :address =>             ENV['SENDGRID_ADDRESS'],
+    :port =>                ENV['SMTP_PORT'],
+    :authentication =>      ENV['SMTP_AUTHENTICATION'],
+    :openssl_verify_mode => ENV['SMTP_OPENSSL_VERIFY_MODE']
 }
 
 # Initialize the rails application
