2305111d9071e35d00910051274f2a4893e5f438
Anthony M. Midili <tonywinslow@yahoo.com>
2014-03-16 12:00:42 -0400

    Toggle is_owner and is_admin when a user signs up.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+protected
+
+  # Build a devise resource passing in the session. Useful to move
+  # temporary session data to the newly created user.
+  def build_resource(hash=nil)
+    self.resource = resource_class.new_with_session(hash || {}, session)
+    self.resource.is_owner = true
+    self.resource.is_admin = true
+  end
+
 end
