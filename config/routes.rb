f478c96e09121a9845f1df8e810c78c5bfc65eab
Anthony M. Midili <tonywinslow@yahoo.com>
2014-03-16 11:16:35 -0400

    Tell devise to use controllers/users/registrations_controller. Set routes and move views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 HouseKeeping::Application.routes.draw do
-  devise_for :users, :skip => [:registrations], :controllers => { :invitations => 'users/invitations' }
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', as: 'user_registration'
-    delete 'users' => 'devise/registrations#destroy'
-  end
-
+  devise_for :users,
+             :controllers => { :registrations => 'users/registrations', :invitations => 'users/invitations' }
   resource :company do
     member do
       get :quarterly_report
