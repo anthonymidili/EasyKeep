c95c7e543f18e3707563df0f7719cbdff6d81207
Anthony M. Midili <tonywinslow@yahoo.com>
2014-03-16 22:28:46 -0400

    Seed file no longer required. Owners can now sign them selves up to use the app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-user = User.find_or_create_by_email(email: 'gibbs183@yahoo.com')
-user.update_attribute(:is_admin, true)
-user.update_attribute(:is_owner, true)
-if user.encrypted_password.blank?
-  user.update_attributes(password: 'password', name: 'Donna Gibbs')
-end
-
-user = User.find_or_create_by_email(email: 'tonywinslow@yahoo.com')
-user.update_attribute(:is_admin, true)
-user.update_attribute(:is_owner, true)
-if user.encrypted_password.blank?
-  user.update_attributes(password: 'password', name: 'Anthony Midili')
-end
