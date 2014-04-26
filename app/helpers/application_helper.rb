72d8ae5be0fad648d699180de52b1890694eb918
Anthony M. Midili <tonywinslow@yahoo.com>
2014-04-26 19:13:02 -0400

    Move meta description content into en.yml file.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def description_content
-    'EasyKeep - This app is geared for small businesses! Manage your accounts on the go. Create invoices with the push of a button. Invite customers to view their account history. Ideal for businesses with no overhead and provides a service such as house keeping, interior design, lawn care, baby sitting, etc.'
-  end
 end
