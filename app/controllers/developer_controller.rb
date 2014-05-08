0f350f45bcc2bd653a9f1a42c490c0a73ac52ddb
Anthony M. Midili <tonywinslow@yahoo.com>
2014-05-07 22:29:52 -0400

    Developer basic information table.

--- app/controllers/developer_controller.rb
+++ app/controllers/developer_controller.rb
@@ -3,8 +3,14 @@ class DeveloperController < ApplicationController
   before_filter :authenticate_developer!
 
   def dashboard
-    @users = User.all
-    @owners = User.where(is_owner: true)
+    @users = User.page(params[:page]).per(10)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to developer_dashboard_path, alert: 'User successfully deleted.'
   end
 
 private
