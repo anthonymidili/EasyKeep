5966988c3e90d457fb6e1b257b8263ab1d6c3ae9
Anthony M. Midili <tonywinslow@yahoo.com>
2014-06-05 22:11:48 -0400

    Add user_attributes to account_params.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -71,6 +71,7 @@ private
   # Never trust parameters from the scary internet, only allow the white list through.
   def account_params
     params.require(:account).permit(:address_1, :address_2, :city, :fax, :name, :phone, :state, :zip,
-                                    :uses_account_name, :uses_contact_name, :user_attributes)
+                                    :uses_account_name, :uses_contact_name,
+                                    user_attributes: [:email, :password, :password_confirmation, :remember_me, :name])
   end
 end
