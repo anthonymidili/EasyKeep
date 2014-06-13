73ea028cda0e736da18ca83a91e562195b317bd4
Anthony M. Midili <tonywinslow@yahoo.com>
2014-06-12 22:54:48 -0400

    Add name to devise parameters for invites.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,5 +68,6 @@ protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:invite) << :name
   end
 end
