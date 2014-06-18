e22d7dc36cd2e3dd79d7984730a3c95f62203591
Anthony M. Midili <tonywinslow@yahoo.com>
2014-06-17 22:24:28 -0400

    Refactor developer controller / dashboard @users.

--- app/controllers/developer_controller.rb
+++ app/controllers/developer_controller.rb
@@ -3,16 +3,16 @@ class DeveloperController < ApplicationController
   before_action :authenticate_developer!
 
   def dashboard
-    if params[:all_users]
-      @users = Kaminari.paginate_array(User.all.sort_by(&:company_id)).page(params[:page]).per(10)
-    else
-      @users = Kaminari.paginate_array(User.by_admin_user.sort_by(&:company_id)).page(params[:page]).per(10)
-    end
+    @users = Kaminari.paginate_array(admins_or_all_users.sort_by(&:company_id)).page(params[:page]).per(10)
   end
 
 private
 
   def authenticate_developer!
     redirect_to root_path unless current_user.email == ENV['DEVELOPER_EMAIL']
+  end
+
+  def admins_or_all_users
+    params[:all_users] ? User.all : User.by_admin_user
   end
 end
