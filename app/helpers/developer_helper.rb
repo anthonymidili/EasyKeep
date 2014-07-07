6aa13eab9a618bceba6921485bfbf36669816bb4
anthonymidili <tonywinslow@yahoo.com>
2014-07-06 20:01:30 -0400

    Create mock dashboard views.

--- app/helpers/developer_helper.rb
+++
@@ -1,9 +0,0 @@
-module DeveloperHelper
-  def admin_or_all_users
-    if params[:all_users]
-      link_to 'Show admin users', developer_dashboard_path
-    else
-      link_to 'Show all users', all_users_path(:all_users)
-    end
-  end
-end
