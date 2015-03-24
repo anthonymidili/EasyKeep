fceb576658515ce79e1345aa5585480e309cb956
anthonymidili <tonywinslow@yahoo.com>
2015-03-23 22:40:43 -0400

    Load @account in accounts controller.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,13 +3,13 @@ class AccountsController < ApplicationController
   before_action :require_owner!, only: [:destroy]
   before_action :require_admin!, except: [:edit, :update, :destroy]
   before_action :authenticate_account!, except: [:index, :new, :create]
+  before_action :load_account, only: [:show, :edit, :update, :destroy, :invite_customer]
 
   def index
     @accounts = current_company.accounts.by_name.page(params[:page]).per(10)
   end
 
   def show
-    @account = current_account
     @service = @account.services.build
     @service.performed_on ||= Date.current
     @services = @account.services
@@ -36,11 +36,10 @@ class AccountsController < ApplicationController
   end
 
   def edit
-    @account = current_account
+
   end
 
   def update
-    @account = current_account
     @account.user.skip_validation = true
 
     if @account.update_attributes(account_params)
@@ -51,18 +50,31 @@ class AccountsController < ApplicationController
   end
 
   def destroy
-    @account = current_account
     @account.destroy
     redirect_to accounts_url, alert: 'Account and all account information was successfully deleted.'
   end
 
   def invite_customer
-    @account = current_account
     @account.user.invite!(current_user)  # current user is optional to set the invited_by attribute
     redirect_to account_path(@account), notice: 'Successfully sent invitation.'
   end
 
 private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def account_params
+    params.require(:account).permit(:address_1, :address_2, :city, :fax, :name, :phone, :state, :zip,
+                                    :uses_account_name, :uses_contact_name,
+                                    user_attributes: [:id, :email, :password, :password_confirmation, :remember_me, :name])
+  end
+
+  def authenticate_account!
+    redirect_to root_path if current_account.nil?
+  end
+
+  def load_account
+    @account = current_account
+  end
 
   def redirect_account_or_dashboard
     if current_user.is_admin?
@@ -70,18 +82,5 @@ private
     else
       redirect_to root_path, notice: 'Your account was successfully updated.'
     end
-  end
-
-  def authenticate_account!
-    if current_account.nil?
-      redirect_to root_path
-    end
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def account_params
-    params.require(:account).permit(:address_1, :address_2, :city, :fax, :name, :phone, :state, :zip,
-                                    :uses_account_name, :uses_contact_name,
-                                    user_attributes: [:id, :email, :password, :password_confirmation, :remember_me, :name])
   end
 end
