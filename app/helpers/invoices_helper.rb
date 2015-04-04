5db95af96c2259d823f026c7bb9310edbdf1456c
anthonymidili <tonywinslow@yahoo.com>
2015-04-04 13:28:20 -0400

    Creat css method to change text color to red if action is required on an account.

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,14 +1,11 @@
 module InvoicesHelper
-  def red_text?(item)
-    'red_text' unless item.paid_in_full?
-  end
-
   def pif_or_balance_due(item)
-    if item.paid_in_full?
-      'P. I. F.'
-    else
-      link_to_if current_user.is_admin?, number_to_currency(item.balance_due), new_invoice_payment_path(item),
-              title: 'Apply Payment', remote: true
+    case item.paid_in_full?
+      when true
+        'P. I. F.'
+      else
+        link_to_if current_user.is_admin?, number_to_currency(item.balance_due), new_invoice_payment_path(item),
+                   title: 'Apply Payment', remote: true
     end
   end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -2,11 +2,11 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-preload_app!
-
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
+
+preload_app!
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
