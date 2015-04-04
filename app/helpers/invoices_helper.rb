be63cb5f8c0bea230d90a22929977d7e8c571233
anthonymidili <tonywinslow@yahoo.com>
2015-04-04 18:44:22 -0400

    Refactor invoice helper methods.

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,20 +1,25 @@
 module InvoicesHelper
   def pif_or_balance_due(invoice)
-    case invoice.paid_in_full?
-    when true
-      'P. I. F.'
-    else
-      link_to_if current_user.is_admin?, number_to_currency(invoice.balance_due), new_invoice_payment_path(invoice),
-                 title: 'Apply Payment', remote: true
-    end
+    link = link_to_if current_user.is_admin?, number_to_currency(invoice.balance_due), new_invoice_payment_path(invoice),
+               title: 'Apply Payment', remote: true
+
+    true_or_false(invoice, link)
   end
 
   def pif_or_invoice(invoice)
+    link = link_to number_to_currency(invoice.balance_due), invoice_path(invoice, account_id: invoice.account_id)
+
+    true_or_false(invoice, link)
+  end
+
+private
+
+  def true_or_false(invoice, link)
     case invoice.paid_in_full?
     when true
       'P. I. F.'
     else
-      link_to number_to_currency(invoice.balance_due), invoice_path(invoice, account_id: invoice.account_id)
+      link
     end
   end
 end
