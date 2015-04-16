8e2bb7eee81c4fae8fb1ad485bca1c74a06bf9c7
anthonymidili <tonywinslow@yahoo.com>
2015-04-16 10:45:33 -0400

    Only show the account pre_post_fields if current user is admin.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,15 @@ module UsersHelper
       'No'
     end
   end
+
+  def edit_email_address(account_user)
+    if account_user.encrypted_password.present? && current_user.is_admin?
+      'Only the customer can change their email address once they are an Active user.'
+    elsif account_user.encrypted_password.present? && account_user == current_user
+      'Email can be edited in User/User Settings'
+    else
+      "Once a customer becomes an Active user, admins will not be able to edit the customer's Email. This is to prevent the customer from not being able to log on to their account."
+    end
+  end
+
 end
