3c875867eb19576a0cd336d04b6559734e37eebb
anthonymidili <tonywinslow@yahoo.com>
2015-04-17 17:35:48 -0400

    Delete used migration files.

--- db/migrate/20150413223316_add_number_to_invoices.rb
+++
@@ -1,9 +0,0 @@
-class AddNumberToInvoices < ActiveRecord::Migration
-  def change
-    add_column :invoices, :number, :integer
-
-    Invoice.all.each do |invoice|
-      invoice.update_attribute(:number, invoice.id)
-    end
-  end
-end
--- db/migrate/20150416002358_add_pre_post_fix_to_accounts.rb
+++
@@ -1,7 +0,0 @@
-class AddPrePostFixToAccounts < ActiveRecord::Migration
-  def change
-    add_column :accounts, :prefix, :string
-    add_column :accounts, :postfix, :string
-    add_column :accounts, :divider, :string
-  end
-end
