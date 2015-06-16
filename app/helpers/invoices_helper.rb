a8d4cbe62c4340411a9b46ec138d2971e3306618
Anthony Midili <tonywinslow@yahoo.com>
2015-06-15 22:02:50 -0400

    Create red links for delete / destroy actions.

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,7 +1,8 @@
 module InvoicesHelper
   def pif_or_balance_due(invoice)
-    link = link_to_if current_user.is_admin?, number_to_currency(invoice.balance_due), new_invoice_payment_path(invoice),
-               title: 'Apply Payment', remote: true
+    link = link_to_if current_user.is_admin?, number_to_currency(invoice.balance_due),
+                      new_invoice_payment_path(invoice),
+                      title: 'Apply Payment', remote: true
 
     true_or_false(invoice, link)
   end
