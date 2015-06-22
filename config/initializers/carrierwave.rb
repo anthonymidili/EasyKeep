7d9afe9661c2da34b8bfc1573ccb779442666f11
Anthony Midili <tonywinslow@yahoo.com>
2015-06-22 18:00:44 -0400

    Merge branch 'frankjmattia-issue-60' into easykeep310-wip
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-      # Configuration for Amazon S3 should be made available through an Environment variable.
-      # For local installations, export the env variable through the shell OR
-      # if using Passenger, set an Apache environment variable.
-      #
-      # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-      #
-      # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
+    # Configuration for Amazon S3 should be made available through an Environment variable.
+    # For local installations, export the env variable through the shell OR
+    # if using Passenger, set an Apache environment variable.
+    #
+    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
+    #
+    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
 
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
+    # Configuration for Amazon S3
+    :provider              => 'AWS',
+    :aws_access_key_id     => ENV.fetch('AWS_ACCESS_KEY_ID', ''),
+    :aws_secret_access_key => ENV.fetch('AWS_SECRET_KEY', '')
   }
 
   # For testing, upload files to local `tmp` folder.
