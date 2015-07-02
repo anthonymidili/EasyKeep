2a4ad2c981dc92697aeb3479861adf13d658a607
Anthony Midili <tonywinslow@yahoo.com>
2015-07-02 17:56:45 -0400

    Remove unused Test folder.

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Accounts', type: :request do
   describe 'GET /accounts' do
-    it 'works! (now write some real specs)' do
+    it 'Account index' do
       get accounts_path
       expect(response).to have_http_status(302)
     end
